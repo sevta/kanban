e32aadfdaa56cc602a444ad9d1262237e51d3ecc
septa <sevtasaputra@gmail.com>
2023-01-07 12:43:34 +0700

    feat: implement sign up, add pkg notification system

--- components/ModalSignUp.tsx
+++ components/ModalSignUp.tsx
@@ -6,62 +6,80 @@ import {
   Stack,
   TextInput,
 } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { useForm, zodResolver } from "@mantine/form";
+import axios from "axios";
 import { ModalBaseProps } from "types";
+import { apiUrl } from "utils";
+import { z } from "zod";
 
-type SignUpForm = {
-  name: string;
-  email: string;
-  password: string;
-  password_confirmation: string;
-};
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email({ message: "invalid email" }),
+  password: z.string().min(6),
+  password_confirmation: z.string().min(6),
+});
+
+type SignUpType = z.infer<typeof schema>;
 
 export default function ModalSignUp({
-  onCancel,
-  onSubmit,
   onSignIn,
 }: { onSignIn?: () => void } & ModalBaseProps) {
-  const form = useForm<SignUpForm>({
+  const form = useForm<SignUpType>({
     initialValues: {
       name: "",
       email: "",
       password: "",
       password_confirmation: "",
     },
+
+    validate: zodResolver(schema),
   });
+
+  async function handleSubmit(values: SignUpType) {
+    try {
+      const resp = await axios.post(apiUrl + "signup", {
+        ...values,
+      });
+      console.log({ resp });
+    } catch (error) {
+      console.log({ error });
+    }
+  }
 
   return (
     <Box>
-      <Stack>
-        <TextInput
-          placeholder="Name"
-          label="Name"
-          {...form.getInputProps("name")}
-        />
-        <TextInput
-          placeholder="Email"
-          label="Email"
-          type="email"
-          {...form.getInputProps("email")}
-        />
-        <PasswordInput
-          placeholder="Password"
-          label="Password"
-          {...form.getInputProps("password")}
-        />
-        <PasswordInput
-          placeholder="Password"
-          label="Password"
-          {...form.getInputProps("password_confirmation")}
-        />
-      </Stack>
-      <Stack mt="lg">
-        <Button onClick={() => onSubmit?.()}>Sign Up</Button>
-        <Divider label="or" labelPosition="center" />
-        <Button variant="light" onClick={() => onSignIn?.()}>
-          Sign In
-        </Button>
-      </Stack>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <Stack>
+          <TextInput
+            placeholder="Name"
+            label="Name"
+            {...form.getInputProps("name")}
+          />
+          <TextInput
+            placeholder="Email"
+            label="Email"
+            type="email"
+            {...form.getInputProps("email")}
+          />
+          <PasswordInput
+            placeholder="Password"
+            label="Password"
+            {...form.getInputProps("password")}
+          />
+          <PasswordInput
+            placeholder="Password"
+            label="Password"
+            {...form.getInputProps("password_confirmation")}
+          />
+        </Stack>
+        <Stack mt="lg">
+          <Button type="submit">Sign Up</Button>
+          <Divider label="or" labelPosition="center" />
+          <Button variant="light" onClick={() => onSignIn?.()}>
+            Sign In
+          </Button>
+        </Stack>
+      </form>
     </Box>
   );
 }
